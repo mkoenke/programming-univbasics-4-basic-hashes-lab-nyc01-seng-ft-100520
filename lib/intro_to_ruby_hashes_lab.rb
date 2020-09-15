81c0ee241cc80e38cc76d93fe26c217860d753d3
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-15 12:15:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,19 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
+  {:key => "value"}
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  
 end
